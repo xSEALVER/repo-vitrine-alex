c91572c87d7f65d0c3fbe2dcf8eac02feceb24ed
xSEALVER <miezaneric2@gmail.com>
2025-01-17 09:18:18 +0100

    sec commit

--- vitrine-alex/src/Components/Hero.jsx
+++ vitrine-alex/src/Components/Hero.jsx
@@ -9,8 +9,8 @@ const Hero = () => {
 
             {/* Overlay Text */}
             <div className="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50">
-                <h1 className="text-white text-4xl md:text-6xl font-bold mb-4">Your Journey, Your Story</h1>
-                <p className="text-white text-lg mb-6">Choose Your Favourite Destination.</p>
+                <h1 className="text-white text-4xl md:text-6xl font-bold mb-4 w-" >L'avenir de la mobilité est là</h1>
+                <p className="text-white text-lg mb-6">Avec Aude Mobilté, trouvez des solutions de transport adaptés à vos besoins.</p>
                 
             </div>
         </div>
